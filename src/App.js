41714ae5946423c6806ee47b59a839c3c71deb07
USERNAME <EMAIL>
2023-03-27 19:14:55 -0400

    Changed Favicon and added some components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Nav from "./Components/Nav"
+import Home from "./Components/Home";
+// import Map from "./Components/Map"
+
+
 
 function App() {
   return (
     <div className="App">
+      {/* <Nav /> */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+        </Routes>
+
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My weather App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
