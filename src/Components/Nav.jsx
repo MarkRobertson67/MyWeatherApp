41c72b67e24e64227338e0fa5309424449047e66
USERNAME <EMAIL>
2023-03-27 21:38:31 -0400

    Initial setup complete with 3 linked pages

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -5,7 +5,25 @@ import "./Nav.css";
 function Nav() {
     return (
 
-        <nav className="navbar"></nav>
 
+        <nav className="navbar">
+        <ul className="navbarlinks">
+            <li>
+            <Link to="/">Home</Link>
+            </li>
+            <li>
+            <Link to="/map">Map</Link>
+            </li>
+
+            <li>
+            <Link to="/about">About</Link>
+            </li>
+        
+        
+        </ul>
+
+        </nav>
     )
 }
+
+export default Nav;
