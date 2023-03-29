0d65c48a6229ba96f933fd6d9f3823f51fbf7a15
USERNAME <EMAIL>
2023-03-28 21:18:30 -0400

    added temp conversion

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,16 +1,54 @@
-// import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./Home.css"
 
 function Home({ weatherData }) {
 
-  
+    const [conversionResult, setConversionResult] = useState(null);
+
+    const handleConversionSubmit = (event) => {
+        event.preventDefault();
+        const conversionValue = event.target.convert.value;
+        const isToCelsius = event.target.conversionType.value === "toCelsius";
+        const result = isToCelsius
+          ? ((conversionValue - 32) * 5) / 9
+          : conversionValue * (9 / 5) + 32;
+        setConversionResult({ value: result.toFixed(2), isToCelsius });
+      };
+
 
     return (
     <div className="container">
       <header className="header">Weather App</header>
       <main className="main.container">
-       
-        
+      <section className="left-container">
+          <h2>Convert Temperature</h2>
+          <form id="conversionForm" onSubmit={handleConversionSubmit}>
+            <label>
+              <input type="radio" name="conversionType" value="toCelsius" />
+              Fahrenheit to Celsius
+            </label>
+            <label>
+              <input type="radio" name="conversionType" value="toFahrenheit" />
+              Celsius to Fahrenheit
+            </label>
+            <input type="number" name="convert" />
+            <button type="submit">Convert</button>
+          </form>
+          {conversionResult && (
+            <div>
+              <p>
+                {conversionResult.isToCelsius
+                  ? `${conversionResult.value}°C`
+                  : `${conversionResult.value}°F`}
+              </p>
+              <button onClick={() => setConversionResult(null)}>
+                Clear Conversion
+              </button>
+            </div>
+          )}
+        </section>
+
+        <section className="center-container">
         {weatherData && (
           <div>
             <h1>{weatherData.nearest_area[0].areaName[0].value}</h1>
@@ -21,6 +59,7 @@ function Home({ weatherData }) {
             <p>Current temperature: {weatherData.current_condition[0].temp_F}°F</p>
           </div>
         )}
+        </section>
       </main>
     </div>
   );
